120a094cd8128cc409166a93e27b95ab747ba914
desireealexia <desireealexia19@gmail.com>
2025-03-03 18:17:38 +0000

    Customised UserAdmin to remove user permissions from admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import User, Review, Watchlist
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Review, Watchlist
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        ('Personal info', {'fields': ('username', 'password','email', 'date_joined')}),
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Review)
 admin.site.register(Watchlist)
